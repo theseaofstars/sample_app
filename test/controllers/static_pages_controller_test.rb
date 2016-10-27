619e87bec6f0de4cfe0ddfdbc2577e0708a12587
theseaofstars <theseaofstars@live.cn>
2016-10-27 20:29:22 +0800

    finish chapter 5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,20 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "home"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "help"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "about"
   end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "contact"
+  end
+
 end
