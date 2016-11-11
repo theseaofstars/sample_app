1c6dc32bd0cc7595321997aa4a8e46d33e29852c
theseaofstars <theseaofstars@live.cn>
2016-11-11 13:52:31 +0800

    finish 10.2

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -45,20 +45,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email,@user.email
   end
 
-  test "should redirect edit when not logged in" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "should redirect update when not logged in" do
-    patch user_path(@user),params: {
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: {
         user: {
-            name: @user.name,
-            email: @user.email
+            name: name,
+            email: email,
+            password: "",
+            password_confirmation: ""
         }
     }
     assert_not flash.empty?
-    assert_redirected_to login_url
+    assert_redirected_to user_path(@user)
+    @user.reload
+    assert_equal name,@user.name
+    assert_equal email,@user.email
   end
+
+
 end
