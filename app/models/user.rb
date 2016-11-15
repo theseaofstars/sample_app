8c7b513bcf8368a65a34f28bf21f6f32a1408f9a
theseaofstars <theseaofstars@live.cn>
2016-11-15 22:20:07 +0800

    finish 11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token,:activation_token
   before_save {email.downcase!}
+  before_create :create_activation_digest
   validates(:name,presence: true,length:{maximum: 50})
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email,presence: true,length: {maximum: 255},
@@ -35,4 +36,10 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
+
+  private
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
