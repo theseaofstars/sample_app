63130d4290c9b133ac4fe55cf7913a463822a34f
theseaofstars <theseaofstars@live.cn>
2016-11-21 22:04:24 +0800

    finish 13.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token,:activation_token,:reset_token
   before_save {email.downcase!}
   before_create :create_activation_digest
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,12 @@ foo@bar+baz.com]
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember,'')
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "test")
+    assert_difference 'Micropost.count',-1 do
+      @user.destroy
+    end
+  end
 end
