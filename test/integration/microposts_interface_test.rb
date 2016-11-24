fa90ac2e534bb62dd78d5f81291263a2e7a61ab5
theseaofstars <theseaofstars@live.cn>
2016-11-24 22:00:42 +0800

    finish 13.3

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_difference 'Micropost.count',1 do
+      post microposts_path,params:{
+          micropost:{
+              content:"valid content"
+          }
+      }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match "valid content",response.body
+
+    assert_no_difference 'Micropost.count' do
+      post microposts_path,params:{
+          micropost:{
+              content:""
+          }
+      }
+    end
+    assert_select 'div#error_explanation'
+    get root_url
+    assert_select 'a',text:"delete"
+    first_micropost = @user.microposts.paginate(page:1).first
+    assert_difference 'Micropost.count',-1 do
+      delete micropost_path(first_micropost)
+    end
+    get user_path(users(:archer))
+    assert_select 'a',text:'delete',count:0
+  end
+end
