043682ac1bfb42afe269d0a2602b28dae8440d40
theseaofstars <theseaofstars@live.cn>
2016-11-27 17:59:46 +0800

    finish 14.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -20,13 +20,19 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
 
-  resources :users
+  resources :users do
+    member do
+      get :following,:followers
+    end
+  end
 
   resources :account_activations, only: [:edit]
 
   resources :password_resets, only: [:new,:edit,:create,:update]
 
   resources :microposts, only: [:create,:destroy]
+
+  resources :relationships, only: [:create,:destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,10 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each {|user| user.microposts.create!(content:content)}
 end
+
+users = User.all
+user = users.first
+following = users[2..50]
+follower = users[3..40]
+following.each{|followed| user.follow(followed)}
+follower.each{|follower| follower.follow(user)}
